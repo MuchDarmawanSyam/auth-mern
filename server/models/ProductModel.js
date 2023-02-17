53053d1a8e80ff268e2e70c6a79b16446fd5a807
Much Darmawan Iriansyah Syam <mchdarmawan@gmail.com>
2023-02-17 19:34:08 +0900

    Membuat api user dan product serta frontend react redux

---
+++ server/models/ProductModel.js
@@ -0,0 +1,46 @@
+import { Sequelize } from "sequelize";
+import db from "../config/database.js";
+import Users from "./UserModel.js"
+
+const {DataTypes} = Sequelize;
+
+const Products = db.define('product', {
+    uuid: {
+        type: DataTypes.STRING,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false, // field uuid tidak boleh bernilai null
+        validate: {
+            notEmpty: true // field uuid tidak boleh bernilai empty string
+        }
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false, // field uuid tidak boleh bernilai null
+        validate: {
+            notEmpty: true, // field uuid tidak boleh bernilai empty string
+            len: [3, 100] // jumlah karakter di field name
+        }
+    },
+    price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    }
+}, {
+    freezeTableName: true
+});
+
+// Relasi One to Many (Satu user bisa menginput banyak product)
+Users.hasMany(Products);
+Products.belongsTo(Users, {foreignKey: 'userId'});
+
+export default Products;
